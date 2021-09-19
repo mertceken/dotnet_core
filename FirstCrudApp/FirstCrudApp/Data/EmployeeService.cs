201acd08b1804b3c68fd5720848d892fa69912fb
Mert Çeken <mertceken12@gmail.com>
2021-09-19 14:44:08 +0300

    Crud App with Blazor
    Here is the my first crud app using blazor without database connection

---
+++ FirstCrudApp/FirstCrudApp/Data/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCrudApp.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private List<Employee> employeeList = new List<Employee>()
+        {
+            new Employee{
+            Id =Guid.NewGuid(),
+            Name="John Doe"
+            },
+            new Employee
+            {
+            Id= Guid.NewGuid(),
+            Name="Christian"
+            }
+
+        };
+
+        public void AddEmployee(Employee model)
+        {
+            var id = Guid.NewGuid();
+            model.Id = id;
+            employeeList.Add(model);
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            var employee = GetEmployee(id);
+            employeeList.Remove(employee);
+
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employeeList.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employeeList;
+        }
+
+        public void UpdateEmployee(Employee model)
+        {
+            var getOldEmpName = GetEmployee(model.Id);
+            getOldEmpName.Name = model.Name;
+        }
+    }
+}
---
+++ FirstCrudApp/FirstCrudApp/Data/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCrudApp.Data
+{
+    interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee(Employee model);
+
+        void AddEmployee(Employee model);
+
+        void DeleteEmployee(Guid id);
+    }
+}
