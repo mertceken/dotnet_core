a18254b31456d6f02cae6b27c24881710207ecc2
Mert Çeken <mertceken12@gmail.com>
2021-10-31 15:40:08 +0300

    Authorization Tutorial
    In this project, I want to try perform role based Authorization in ASP.NET Core MVC with simple project.

---
+++ AuthorizationTutorial/AuthorizationTutorial/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthorizationTutorial.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize(Roles ="Admin,User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles ="Admin,User")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username,string password)
+        {
+            if (!string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+            {
+                return RedirectToAction("Login");
+            }
+            ClaimsIdentity identity = null;
+            bool isAuthenticate = false;
+            if (username=="demo" && password=="c")
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,username),
+                    new Claim(ClaimTypes.Role,"User")
+                },CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticate = true;
+            }
+            if (isAuthenticate)
+            {
+                var principal = new ClaimsPrincipal(identity);
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ AuthorizationTutorial/AuthorizationTutorial/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AuthorizationTutorial.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Authorize(Roles = "Admin,User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin,User")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
