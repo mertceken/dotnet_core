a47492738af9f0c58217721646a23fd9c6d432de
Mert Çeken <mertceken12@gmail.com>
2021-11-28 01:48:15 +0300

    Microservice Tutorial
    Using ASP.NET Core, Ocelot API Gateway.

---
+++ MicroserviceTutorial/UserService/Model/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Entities;
+
+namespace UserService.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=UserDB;Integrated Security=True");
+
+        }
+            
+    }
+}
